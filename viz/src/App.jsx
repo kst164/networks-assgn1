5b47309bbfed78b3ec58f9c69efc833e620855a2
Kushagra Gupta <kushagra.gupta0103@gmail.com>
2023-08-25 02:06:31 +0530

    change format of json data, add length of edge ref
    add 3d package just in case

--- viz/src/App.jsx
+++ viz/src/App.jsx
@@ -1,29 +1,72 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect, useRef } from 'react'
 import './App.css'
 import ForceGraph2D from 'react-force-graph-2d';
-
+import ForceGraph3D from 'react-force-graph-3d';
 function App() {
-  const [count, setCount] = useState(0)
   const [data, setData] = useState(null)
+  const graphRef = useRef(null);
+
   useEffect(() => {
     fetch('data.json').then(res => res.json()).then(data => {
       console.log(data);
       setData(data);
     })
   }, [])
+  // useEffect(() => {
+  //   if (data) {
+  //     graphRef.current.d3Force('link').distance(
+  //       d => d.value * 100
+  //     )
+  //   }
+  // }, [data])
+  const speedFactor = 0.001;
+
+  const colors = {
+    1: 'red',
+    2: 'blue',
+    3: 'green',
+    4: 'yellow',
+    5: 'orange',
+    6: 'purple',
+    7: 'pink',
+    8: 'brown',
+    9: 'grey',
+    10: 'purple',
+  }
+
+
   return (
     <>
       <div>
         {data && (
+          <>
           <ForceGraph2D
             graphData={data}
             nodeLabel="id"
             nodeAutoColorBy="group"
             linkDirectionalParticles="value"
-            linkDirectionalParticleSpeed={d => d.value * 0.001}
+            linkDirectionalParticleSpeed={d => (d.value + d.value*(Math.random()*0.2 - 0.1)) * speedFactor}
+            linkDirectionalParticleColor={d=> colors[d.color]}
+            linkLabel={d => d.label}
+            ref={graphRef}
+            // linkDirectionalArrowLength={d => d.value * 2}
+            // linkCurvature={0.1}
           />
+          {/* <ForceGraph3D
+            graphData={data}
+            nodeLabel="id"
+            nodeAutoColorBy="group"
+            linkDirectionalParticles="value"
+            linkDirectionalParticleSpeed={d => (d.value + d.value*(Math.random()*0.2 - 0.1)) * speedFactor}
+            linkDirectionalParticleWidth={4}
+            linkDirectionalParticleColor={d => colors[d.value]}
+            linkLabel={d => d.value}
+            ref={graphRef}
+            // linkDirectionalArrowLength={d => d.value * 2}
+            // linkAutoColorBy={d => colors[d.value]}
+            // linkCurvature={0.1}
+            /> */}
+          </>
         )}
       </div>
     </>
