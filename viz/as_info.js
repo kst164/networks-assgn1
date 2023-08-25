cf21407c6d214c863cc1047a8266912844c676f7
Kushagra Gupta <kushagra.gupta0103@gmail.com>
2023-08-25 23:39:33 +0530

    final?

--- viz/as_info.js
+++ viz/as_info.js
@@ -1,84 +1,36 @@
 export const as_info = {
+    "59193": {
+        "shortname": "IIT Hyderabad",
+        "cidr_list": [
+            "103.232.241.0/24"
+        ],
+        "country": "IN"
+    },
     "9498": {
         "shortname": "Bharti Airtel Limited",
         "cidr_list": [
+            "182.79.164.0/24",
             "116.119.32.0/19",
-            "182.79.201.0/24",
-            "182.79.161.0/24",
-            "182.79.245.0/24",
             "116.119.64.0/18",
+            "182.79.245.0/24",
             "59.144.94.0/24",
             "182.79.134.0/24",
-            "202.56.234.0/24",
+            "182.79.201.0/24",
             "182.71.238.0/24",
-            "182.79.164.0/24",
+            "182.79.161.0/24",
+            "202.56.234.0/24",
+            "182.79.223.0/24",
             "182.79.239.0/24",
-            "182.66.64.0/20",
-            "182.79.223.0/24"
+            "182.66.64.0/20"
         ],
         "country": "IN"
     },
-    "6461": {
-        "shortname": "Zayo Bandwidth",
+    "20965": {
+        "shortname": "GEANT Vereniging",
         "cidr_list": [
-            "64.124.0.0/15",
-            "94.31.0.0/18"
-        ],
-        "country": "US"
-    },
-    "1103": {
-        "shortname": "SURF B.V.",
-        "cidr_list": [
-            "145.144.0.0/12"
+            "62.40.96.0/19"
         ],
         "country": "NL"
-    },
-    "786": {
-        "shortname": "Jisc Services Limited",
-        "cidr_list": [
-            "146.97.0.0/16",
-            "131.111.0.0/16",
-            "193.60.0.0/14",
-            "128.232.0.0/16"
-        ],
-        "country": "GB"
-    },
-    "2200": {
-        "shortname": "Renater",
-        "cidr_list": [
-            "193.48.0.0/14",
-            "193.54.0.0/15",
-            "195.220.0.0/16",
-            "195.221.0.0/16"
-        ],
-        "country": "FR"
-    },
-    "55847": {
-        "shortname": "National Knowledge Network",
-        "cidr_list": [
-            "203.110.240.0/21"
-        ],
-        "country": "IN"
-    },
-    "6453": {
-        "shortname": "TATA COMMUNICATIONS (AMERICA) INC",
-        "cidr_list": [
-            "195.219.0.0/16",
-            "64.86.0.0/16",
-            "80.231.0.0/16",
-            "206.82.128.0/20",
-            "216.6.0.0/17"
-        ],
-        "country": "US"
-    },
-    "559": {
-        "shortname": "SWITCH",
-        "cidr_list": [
-            "130.59.0.0/16",
-            "192.33.92.0/22",
-            "129.132.0.0/16"
-        ],
-        "country": "CH"
     },
     "11537": {
         "shortname": "Internet2",
@@ -88,16 +40,149 @@ export const as_info = {
         ],
         "country": "US"
     },
-    "13335": {
-        "shortname": "Cloudflare, Inc.",
+    "6509": {
+        "shortname": "CANARIE Inc",
         "cidr_list": [
-            "104.16.0.0/20",
-            "172.68.39.0/24",
-            "172.70.216.0/24",
-            "162.158.226.0/23",
-            "172.69.82.0/24",
-            "172.71.196.0/24",
-            "172.69.74.0/24"
+            "205.189.32.0/24"
+        ],
+        "country": "CA"
+    },
+    "54113": {
+        "shortname": "Fastly, Inc.",
+        "cidr_list": [
+            "151.101.0.0/22",
+            "151.101.64.0/22",
+            "151.101.128.0/22",
+            "151.101.156.0/22",
+            "151.101.152.0/22",
+            "199.232.20.0/22",
+            "23.185.0.0/24",
+            "151.101.192.0/22"
+        ],
+        "country": "US"
+    },
+    "2018": {
+        "shortname": "TENET (The UNINET Project)",
+        "cidr_list": [
+            "155.232.0.0/17",
+            "154.114.0.0/17",
+            "155.232.0.0/16"
+        ],
+        "country": "ZA"
+    },
+    "4538": {
+        "shortname": "China Education and Research Network Center",
+        "cidr_list": [
+            "202.112.0.0/16",
+            "118.229.0.0/21",
+            "118.229.8.0/21",
+            "101.4.0.0/14",
+            "166.111.4.0/24"
+        ],
+        "country": "CN"
+    },
+    "8075": {
+        "shortname": "Microsoft Corporation",
+        "cidr_list": [
+            "104.40.0.0/13",
+            "20.192.0.0/10"
+        ],
+        "country": "US"
+    },
+    "2200": {
+        "shortname": "Renater",
+        "cidr_list": [
+            "193.48.0.0/14",
+            "195.221.0.0/16",
+            "195.220.0.0/16",
+            "193.54.0.0/15"
+        ],
+        "country": "FR"
+    },
+    "132423": {
+        "shortname": "Indian Institute of Technology Bombay",
+        "cidr_list": [
+            "103.21.124.0/24"
+        ],
+        "country": "IN"
+    },
+    "559": {
+        "shortname": "SWITCH",
+        "cidr_list": [
+            "192.33.92.0/22",
+            "130.59.0.0/16",
+            "129.132.0.0/16"
+        ],
+        "country": "CH"
+    },
+    "209242": {
+        "shortname": "Cloudflare London, LLC",
+        "cidr_list": [
+            "141.193.213.0/24"
+        ],
+        "country": "US"
+    },
+    "6453": {
+        "shortname": "TATA COMMUNICATIONS (AMERICA) INC",
+        "cidr_list": [
+            "216.6.0.0/17",
+            "195.219.0.0/16",
+            "64.86.0.0/16",
+            "80.231.0.0/16",
+            "206.82.128.0/20"
+        ],
+        "country": "US"
+    },
+    "786": {
+        "shortname": "Jisc Services Limited",
+        "cidr_list": [
+            "146.97.0.0/16",
+            "128.232.0.0/16",
+            "131.111.0.0/16",
+            "193.60.0.0/14"
+        ],
+        "country": "GB"
+    },
+    "1103": {
+        "shortname": "SURF B.V.",
+        "cidr_list": [
+            "145.144.0.0/12"
+        ],
+        "country": "NL"
+    },
+    "36982": {
+        "shortname": "University of Cape Town",
+        "cidr_list": [
+            "137.158.0.0/16"
+        ],
+        "country": "ZA"
+    },
+    "28571": {
+        "shortname": "UNIVERSIDADE DE SAO PAULO",
+        "cidr_list": [
+            "200.144.248.0/24",
+            "143.107.249.0/24"
+        ],
+        "country": "BR"
+    },
+    "55824": {
+        "shortname": "National Knowledge Network",
+        "cidr_list": [
+            "180.149.48.0/24"
+        ],
+        "country": "IN"
+    },
+    "55847": {
+        "shortname": "National Knowledge Network",
+        "cidr_list": [
+            "203.110.240.0/21"
+        ],
+        "country": "IN"
+    },
+    "32": {
+        "shortname": "Stanford University",
+        "cidr_list": [
+            "171.64.0.0/14"
         ],
         "country": "US"
     },
@@ -109,231 +194,31 @@ export const as_info = {
         ],
         "country": "US"
     },
-    "20965": {
-        "shortname": "GEANT Vereniging",
+    "132524": {
+        "shortname": "Tata Institute of Fundamental Research",
         "cidr_list": [
-            "62.40.96.0/19"
-        ],
-        "country": "NL"
-    },
-    "3356": {
-        "shortname": "Level 3 Parent, LLC",
-        "cidr_list": [
-            "4.0.0.0/9",
-            "8.244.0.0/15"
-        ],
-        "country": "US"
-    },
-    "209242": {
-        "shortname": "Cloudflare London, LLC",
-        "cidr_list": [
-            "141.193.213.0/24"
-        ],
-        "country": "US"
-    },
-    "54113": {
-        "shortname": "Fastly, Inc.",
-        "cidr_list": [
-            "151.101.0.0/22",
-            "151.101.128.0/22",
-            "151.101.192.0/22",
-            "151.101.156.0/22",
-            "151.101.64.0/22",
-            "23.185.0.0/24",
-            "199.232.20.0/22",
-            "151.101.152.0/22"
-        ],
-        "country": "US"
-    },
-    "141340": {
-        "shortname": "IIT Madras",
-        "cidr_list": [
-            "103.158.42.0/24"
+            "158.144.1.0/24",
+            "158.144.8.0/23"
         ],
         "country": "IN"
-    },
-    "28571": {
-        "shortname": "UNIVERSIDADE DE SAO PAULO",
-        "cidr_list": [
-            "200.144.248.0/24",
-            "143.107.249.0/24"
-        ],
-        "country": "BR"
-    },
-    "8075": {
-        "shortname": "Microsoft Corporation",
-        "cidr_list": [
-            "104.40.0.0/13",
-            "20.192.0.0/10"
-        ],
-        "country": "US"
-    },
-    "4538": {
-        "shortname": "China Education and Research Network Center",
-        "cidr_list": [
-            "166.111.4.0/24",
-            "118.229.0.0/21",
-            "202.112.0.0/16",
-            "101.4.0.0/14",
-            "118.229.8.0/21"
-        ],
-        "country": "CN"
-    },
-    "2519": {
-        "shortname": "ARTERIA Networks Corporation",
-        "cidr_list": [
-            "222.230.0.0/16",
-            "163.139.0.0/16"
-        ],
-        "country": "JP"
-    },
-    "55824": {
-        "shortname": "National Knowledge Network",
-        "cidr_list": [
-            "180.149.48.0/24"
-        ],
-        "country": "IN"
-    },
-    "4694": {
-        "shortname": "IDC Frontier Inc.",
-        "cidr_list": [
-            "158.205.121.0/24",
-            "210.152.240.0/21"
-        ],
-        "country": "JP"
-    },
-    "376": {
-        "shortname": "Reseau d'Informations Scientifiques du Quebec (RISQ Inc.)",
-        "cidr_list": [
-            "206.167.128.0/17",
-            "192.77.63.0/24"
-        ],
-        "country": "CA"
-    },
-    "59193": {
-        "shortname": "IIT Hyderabad",
-        "cidr_list": [
-            "103.232.241.0/24"
-        ],
-        "country": "IN"
-    },
-    "1124": {
-        "shortname": "Universiteit van Amsterdam",
-        "cidr_list": [
-            "145.18.0.0/16"
-        ],
-        "country": "EU"
-    },
-    "4755": {
-        "shortname": "Tata Communications Limited",
-        "cidr_list": [
-            "14.141.20.0/24",
-            "121.240.0.0/16"
-        ],
-        "country": "IN"
-    },
-    "6509": {
-        "shortname": "CANARIE Inc",
-        "cidr_list": [
-            "205.189.32.0/24"
-        ],
-        "country": "CA"
-    },
-    "23911": {
-        "shortname": "China Next Generation Internet Beijing IX",
-        "cidr_list": [
-            "210.25.189.0/24",
-            "210.25.187.0/24"
-        ],
-        "country": "CN"
     },
     "55836": {
         "shortname": "Reliance Jio Infocomm Limited",
         "cidr_list": [
             "49.44.128.0/17",
             "139.167.44.0/22",
-            "136.232.128.0/19",
-            "115.247.100.0/22",
             "115.247.68.0/22",
-            "115.242.168.0/23"
+            "115.242.168.0/23",
+            "115.247.100.0/22",
+            "136.232.128.0/19"
         ],
         "country": "IN"
     },
-    "132524": {
-        "shortname": "Tata Institute of Fundamental Research",
+    "4755": {
+        "shortname": "Tata Communications Limited",
         "cidr_list": [
-            "158.144.8.0/23",
-            "158.144.1.0/24"
-        ],
-        "country": "IN"
-    },
-    "2018": {
-        "shortname": "TENET (The UNINET Project)",
-        "cidr_list": [
-            "155.232.0.0/16",
-            "154.114.0.0/17"
-        ],
-        "country": "ZA"
-    },
-    "24785": {
-        "shortname": "Broadband Hosting B.V.",
-        "cidr_list": [
-            "217.170.0.0/24"
-        ],
-        "country": "NL"
-    },
-    "36982": {
-        "shortname": "University of Cape Town",
-        "cidr_list": [
-            "137.158.0.0/16"
-        ],
-        "country": "ZA"
-    },
-    "2153": {
-        "shortname": "California State University, Office of the Chancellor",
-        "cidr_list": [
-            "137.164.26.0/23",
-            "137.164.25.0/24"
-        ],
-        "country": "US"
-    },
-    "32": {
-        "shortname": "Stanford University",
-        "cidr_list": [
-            "171.64.0.0/14"
-        ],
-        "country": "US"
-    },
-    "6939": {
-        "shortname": "Hurricane Electric LLC",
-        "cidr_list": [
-            "184.104.0.0/15",
-            "216.218.128.0/17"
-        ],
-        "country": "US"
-    },
-    "132780": {
-        "shortname": "Indian Institute of Technology Delhi",
-        "cidr_list": [
-            "103.27.9.0/24"
-        ],
-        "country": "IN"
-    },
-    "4775": {
-        "shortname": "Globe Telecom (GMCR,INC)",
-        "cidr_list": [
-            "206.72.210.0/23",
-            "80.81.192.0/21",
-            "80.249.208.0/21"
-        ],
-        "country": "PH"
-    },
-    "9829": {
-        "shortname": "Bharat Sanchar Nigam Ltd",
-        "cidr_list": [
-            "210.212.69.0/24",
-            "218.248.0.0/20",
-            "117.236.224.0/20"
+            "14.141.20.0/24",
+            "121.240.0.0/16"
         ],
         "country": "IN"
     },
@@ -344,31 +229,85 @@ export const as_info = {
         ],
         "country": "NL"
     },
-    "1916": {
-        "shortname": "Rede Nacional de Ensino e Pesquisa",
+    "6461": {
+        "shortname": "Zayo Bandwidth",
         "cidr_list": [
-            "200.143.192.0/18"
+            "94.31.0.0/18",
+            "64.124.0.0/15"
         ],
-        "country": "BR"
+        "country": "US"
     },
-    "132423": {
-        "shortname": "Indian Institute of Technology Bombay",
+    "132780": {
+        "shortname": "Indian Institute of Technology Delhi",
         "cidr_list": [
-            "103.21.124.0/24"
-        ],
-        "country": "IN"
-    },
-    "24560": {
-        "shortname": "Bharti Airtel Limited",
-        "cidr_list": [
-            "223.230.16.0/24"
+            "103.27.9.0/24"
         ],
         "country": "IN"
     },
-    "3549": {
+    "23911": {
+        "shortname": "China Next Generation Internet Beijing IX",
+        "cidr_list": [
+            "210.25.189.0/24",
+            "210.25.187.0/24"
+        ],
+        "country": "CN"
+    },
+    "3356": {
         "shortname": "Level 3 Parent, LLC",
         "cidr_list": [
-            "113.29.0.0/20"
+            "8.244.0.0/15",
+            "4.0.0.0/9"
+        ],
+        "country": "US"
+    },
+    "13335": {
+        "shortname": "Cloudflare, Inc.",
+        "cidr_list": [
+            "172.69.74.0/24",
+            "172.71.196.0/24",
+            "162.158.226.0/23",
+            "172.70.216.0/24",
+            "172.69.82.0/24",
+            "104.16.0.0/20",
+            "172.68.39.0/24"
+        ],
+        "country": "US"
+    },
+    "376": {
+        "shortname": "Reseau d'Informations Scientifiques du Quebec (RISQ Inc.)",
+        "cidr_list": [
+            "206.167.128.0/17",
+            "192.77.63.0/24"
+        ],
+        "country": "CA"
+    },
+    "6939": {
+        "shortname": "Hurricane Electric LLC",
+        "cidr_list": [
+            "216.218.128.0/17",
+            "184.104.0.0/15"
+        ],
+        "country": "US"
+    },
+    "15318": {
+        "shortname": "McGill University",
+        "cidr_list": [
+            "132.216.0.0/16"
+        ],
+        "country": "CA"
+    },
+    "141340": {
+        "shortname": "IIT Madras",
+        "cidr_list": [
+            "103.158.42.0/24"
+        ],
+        "country": "IN"
+    },
+    "2153": {
+        "shortname": "California State University, Office of the Chancellor",
+        "cidr_list": [
+            "137.164.26.0/23",
+            "137.164.25.0/24"
         ],
         "country": "US"
     },
@@ -379,11 +318,87 @@ export const as_info = {
         ],
         "country": "IN"
     },
-    "15318": {
-        "shortname": "McGill University",
+    "24785": {
+        "shortname": "Broadband Hosting B.V.",
         "cidr_list": [
-            "132.216.0.0/16"
+            "217.170.0.0/24"
         ],
-        "country": "CA"
+        "country": "NL"
+    },
+    "2519": {
+        "shortname": "ARTERIA Networks Corporation",
+        "cidr_list": [
+            "222.230.0.0/16",
+            "163.139.0.0/16"
+        ],
+        "country": "JP"
+    },
+    "4694": {
+        "shortname": "IDC Frontier Inc.",
+        "cidr_list": [
+            "158.205.121.0/24",
+            "210.152.240.0/21"
+        ],
+        "country": "JP"
+    },
+    "24560": {
+        "shortname": "Bharti Airtel Limited",
+        "cidr_list": [
+            "223.230.16.0/24"
+        ],
+        "country": "IN"
+    },
+    "9829": {
+        "shortname": "Bharat Sanchar Nigam Ltd",
+        "cidr_list": [
+            "218.248.0.0/20",
+            "210.212.69.0/24",
+            "117.236.224.0/20"
+        ],
+        "country": "IN"
+    },
+    "1124": {
+        "shortname": "Universiteit van Amsterdam",
+        "cidr_list": [
+            "145.18.0.0/16"
+        ],
+        "country": "EU"
+    },
+    "4775": {
+        "shortname": "Globe Telecom (GMCR,INC)",
+        "cidr_list": [
+            "206.72.210.0/23",
+            "80.81.192.0/21",
+            "80.249.208.0/21"
+        ],
+        "country": "PH"
+    },
+    "133982": {
+        "shortname": "Excitel Broadband Private Limited",
+        "cidr_list": [
+            "192.140.152.0/24"
+        ],
+        "country": "IN"
+    },
+    "60410": {
+        "shortname": "Hewlett Packard France S.A.S.",
+        "cidr_list": [
+            "16.10.16.0/21"
+        ],
+        "country": "FR"
+    },
+    "3549": {
+        "shortname": "Level 3 Parent, LLC",
+        "cidr_list": [
+            "113.29.0.0/20"
+        ],
+        "country": "US"
+    },
+    "1916": {
+        "shortname": "Rede Nacional de Ensino e Pesquisa",
+        "cidr_list": [
+            "200.143.192.0/18"
+        ],
+        "country": "BR"
     }
 };
--- viz/src/App.jsx
+++ viz/src/App.jsx
@@ -9,8 +9,18 @@ import Select from '@mui/material/Select';
 
 import { as_info } from '../as_info';
 import data from '../data.json';
-const Sources = ["59193", "141340", "132780","132423","1"]
-const Sinks = ["32", "54113","786","209242","13335","4538","559","1124","54113","15318","132524","132423","8075","132780","55847","141340","55479","59193","36982","28571","2200","4694","54113","9829"]
+const Sources = ["59193", "141340", "132780","132423","60410", "133982", "24560", "55847"]
+const Names = {
+  "59193": "IIT Hyderabad",
+  "141340": "IIT Madras",
+  "132780": "Indian Institute of Technology Delhi",
+  "132423": "Indian Institute of Technology Bombay",
+  "60410": "Hewlett Packard France S.A.S.",
+  "133982": "Excitel Broadband Pvt Ltd (Local Bangalore)",
+  "24560": "Bharti Airtel Limited (Local Hyderabad)",
+  "55847": "National Knowledge Network (IIT Guwahati)",
+}
+const Sinks = ["32","54113","786","209242","13335","4538","559","1124","15318","132524","132423","8075","132780","55847","141340","55479","59193","36982","28571","2200","4694"]
 const colors = {
   0: '#6dde8b',
   1: '#2aed0c',
@@ -66,17 +76,19 @@ function App() {
   const [origin, setOrigin] = useState("")
   const [sink, setSink] = useState("")
   
-  useEffect(() => {
-    console.log(origin, sink);
-  }, [origin, sink])
+  // useEffect(() => {
+  //   console.log(origin, sink);
+  // }, [origin, sink])
   
-  
+  const getString = function(cidr_list){
+    return cidr_list.join(", ")
+  }
 
 
   return (
     <Box>
       <Box sx={{ display:"flex", flexDirection:"row", gap:"2rem", justifyContent:"center", padding:"2rem" }}>
-      <FormControl sx={{width: "15rem"}}>
+      <FormControl sx={{width: "20rem"}}>
         <InputLabel id="demo-simple-select-label">Source</InputLabel>
         <Select
           labelId="demo-simple-select-label"
@@ -87,12 +99,12 @@ function App() {
         >
           <MenuItem value={""}>None</MenuItem>
           {as_info && Sources.map((o, i) => {
-            return (<MenuItem key={i} value={o}>{as_info[o]?.shortname}</MenuItem>)
+            return (<MenuItem key={i} value={o}>{Names[o]}</MenuItem>)
           }
           )}
         </Select>
       </FormControl>
-      <FormControl sx={{width: "15rem"}}>
+      <FormControl sx={{width: "20rem"}}>
         <InputLabel id="demo-simple-select-label">Sink</InputLabel>
         <Select
           labelId="demo-simple-select-label"
@@ -102,9 +114,9 @@ function App() {
           onChange={e => setSink(e.target.value)}
         >
           <MenuItem value={""}>None</MenuItem>
-          {Sinks.map((o, i) => (
-            <MenuItem key={i} value={o}>{as_info[o]?.shortname}</MenuItem>
-          ))}
+          {as_info && Sinks.map((o, i) => {
+            return (<MenuItem key={i} value={o}>{as_info[o]?.shortname}</MenuItem>)
+          })}
         </Select>
       </FormControl>
     </Box>
@@ -116,30 +128,31 @@ function App() {
             graphData={data}
             linkSource='from'
             linkTarget='to'
-            nodeLabel={d =>`${d.sname} (ASN: ${d.id})`}
+            nodeLabel={d =>`${d.sname} (ASN: ${d.id})<br/>${getString(d.cidr_list)}`}
             nodeAutoColorBy="country"
+            nodeRelSize={4}
             // linkLabel={d => `${d.value * speedFactor} ms`}
             ref={graphRef}
             backgroundColor='#040D12'
             linkColor={d => {
               if (origin!="" && sink!=""){
-                  if (d.origin == origin && d.sink == sink) {
-                    return '#07f5e9'
-                  }
-                  else{
-                    return '#222424'
-                  }
+                    if (d.origin == origin && d.sink == sink) {
+                      return '#FFFFFF'
+                    }
+                    else{
+                      return '#222424'
+                    }
               }
-              else if (origin!="" || sink!=""){
-                if (d.origin == origin || d.sink == sink) {
-                  return '#07f5e9'
-                }
-                else{
-                  return '#222424'
-                }
+              if (origin!="" || sink!=""){
+                    if (d.origin == origin || d.sink == sink) {
+                      return '#FFFFFF'
+                    }
+                    else{
+                      return '#222424'
+                    }
               }
-              else return '#222424'
-            }}
+              else return '#222424' //colors[d.color]
+              }}
             onNodeDragEnd={node => {
               node.fx = node.x
               node.fy = node.y
@@ -148,7 +161,7 @@ function App() {
             linkWidth={d => {
               if (origin!="" && sink!=""){
                   if (d.origin == origin && d.sink == sink) {
-                    return 3
+                    return 2
                   }
                   else{
                     return 1
@@ -156,7 +169,7 @@ function App() {
               }
               if (origin!="" || sink!=""){
                 if (d.origin == origin || d.sink == sink) {
-                  return 3
+                  return 2
                 }
                 else{
                   return 1
@@ -165,7 +178,21 @@ function App() {
               else return 1
             }}
             linkDirectionalParticles="value"
-            linkDirectionalParticleSpeed={d => d.value * speedFactor}
+            linkDirectionalParticleSpeed={d => {
+              if (origin!="" && sink!=""){
+                if (d.origin == origin && d.sink == sink) {
+                  return d.value * speedFactor
+                }
+                return 0
+                }
+                if (origin!="" || sink!=""){
+                  if (d.origin == origin || d.sink == sink) {
+                    return d.value * speedFactor
+                  }
+                  return 0
+                }
+                else return d.value * speedFactor
+              }} 
             linkDirectionalParticleColor={d => {
               if (origin!="" && sink!=""){
                   if (d.origin == origin && d.sink == sink) {
@@ -182,6 +209,21 @@ function App() {
               else return colors[d.color]
             }}
             linkDirectionalParticleWidth={2}
+            // linkDirectionalArrowLength={d => {
+            //   if (origin!="" && sink!=""){
+            //     if (d.origin == origin && d.sink == sink) {
+            //       return 10
+            //     }
+            //     return 0 
+            //     }
+            //     if (origin!="" || sink!=""){
+            //       if (d.origin == origin || d.sink == sink) {
+            //         return 5
+            //       }
+            //       return 0
+            //     }
+            //     else return 0
+            //   }} 
             // linkCurvature={Math.random()*0.6-0.3}
           />
         )}
