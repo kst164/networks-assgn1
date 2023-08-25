ea5fe752290262a418b7c1bd94af6be14a489143
Kushagra Gupta <kushagra.gupta0103@gmail.com>
2023-08-25 18:13:49 +0530

    new scripts

--- data_collection/edges.py
+++ data_collection/edges.py
@@ -1,24 +1,29 @@
 import json
 
 nodes = json.load(open('nodes.json'))
-
+curr_edges = json.load(open('edges.json'))
+print(len(curr_edges))
 edges = []
 
-all_jsons: list[dict[str, list[dict[str, list[list[dict[int, str, str]]]]]]] = [
+all_jsons = [
     {
         "origin": "blore",
+        "cidr": "pfft",
         "data": json.load(open('clean_traces/blore.json'))
     },
     {
         "origin": "www.iitb.ac.in",
+        "cidr": "103.21.124.0/24",
         "data": json.load(open('clean_traces/iitb.json'))
     },
     {
         "origin": "www.iitd.ac.in",
+        "cidr": "103.27.9.0/24",
         "data": json.load(open('clean_traces/iitd.json'))
     },
     {
         "origin": "www.iith.ac.in",
+        "cidr": "103.232.241.0/24",
         "data": json.load(open('clean_traces/iith.json'))
     },
 ]
@@ -33,6 +38,18 @@ def de_search(s):
 for source in all_jsons:
     x = 0
     for dest in source['data']:
+        tr = dest['traces'][0]
+        for i in range(len(tr) - 1):
+            temp = {
+                "from": source['cidr'],
+                "to": tr[i]['cidr'],
+                "value": abs(0.01 / (tr[i]['rtt'])),
+                "color": x,
+                "source": de_search(source['origin']),
+                "sink": de_search(dest['endpoint'])
+            }
+            edges += [temp]
+        x+=1
         for tr in dest['traces']:
             for i in range(len(tr) - 1):
                 q = {
@@ -52,5 +69,6 @@ for source in all_jsons:
 #     print(f'\t{n},', file=edge_file)
 # print(']', file=edge_file)
 
+print(len(edges))
 with open('edges.json', mode='w') as edge_file:
     json.dump(edges, edge_file, indent=4)
